ec1d4363407e25a82f8c448e4d3fcec0b7b3028c
Kasmi Thapa <kasmithapa00@gmail.com>
2022-08-11 11:40:46 +0545

    string in python

---
+++ new.py
@@ -0,0 +1,11 @@
+#--> It represent a comment in python
+'''
+it represent a multi-line comment
+we can write any thing here
+'''
+
+
+a=5 #here, a is a variable which stores/holds intiger type value i.e 5
+naam="kasmi"#here,naam is a variable which stores/holds string type value i.e "kasmi
+
+#and in python
